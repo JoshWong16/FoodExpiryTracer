7adb27f68056f23dc65c9b7ac5f2b3c653b0e47a
JoshWong16 <40047289+JoshWong16@users.noreply.github.com>
2021-05-11 17:55:24 -0700

    Separated item list and food list

--- src/main/java/com/example/foodtracker/activity3_myitems.java
+++ src/main/java/com/example/foodtracker/activity3_myitems.java
@@ -1,8 +1,10 @@
 package com.example.foodtracker;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -18,6 +22,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.sort;
@@ -29,20 +34,20 @@ public class activity3_myitems extends AppCompatActivity {
     public static final String ITEM_KEY = "item";
     public static final String DATE_KEY = "date";
     public static final String TAG = "test";
-
-
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ArrayList<String> displayList = new ArrayList<>();
-    ListView listView;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    ArrayList<String> itemList = new ArrayList<>();
+    ArrayList<String> dateList = new ArrayList<>();
+    ListView itemListView;
+    ListView dateListView;
     Context context = this;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity3_myitems);
         back2 = (ImageButton) findViewById (R.id.imageButton5);
 
-        listView = (ListView) findViewById(R.id.listview1);
+        itemListView = (ListView) findViewById(R.id.listview1);
+        dateListView = (ListView) findViewById(R.id.listViewID);
 
         back2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,39 +64,8 @@ public class activity3_myitems extends AppCompatActivity {
         }); */
 
         fetchItems();
-
-     /*   db.collection("FoodCollection").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value,
-                                @Nullable FirebaseFirestoreException e) {
-                if (e != null) {
-                    Log.w(TAG, "Listen failed.", e);
-                    return;
-                }
-
-                //Add data from database into FoodList as custom object "FoodItem"
-                ArrayList<FoodItem> FoodList = new ArrayList<>();
-                for (QueryDocumentSnapshot doc : value) {
-                    FoodItem newData = doc.toObject(FoodItem.class);
-                    FoodList.add(newData);
-                }
-
-                sort(FoodList);
-
-                //Make new list of strings for displaying in app
-                displayList = new ArrayList<>();
-                for (FoodItem foodItem : FoodList){
-                    displayList.add(foodItem.getItem() + ": " + foodItem.getStringDate());
-                }
-
-            }
-        });*/
-
-       // ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, displayList);
-
-       // listView.setAdapter(arrayAdapter);
-
     }
+
     public void openactivity5() {
         Intent intent = new Intent(this, activity2.class);
         startActivity(intent);
@@ -101,7 +75,7 @@ public class activity3_myitems extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    
+
     public void fetchItems() {
         db.collection("FoodCollection").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -112,25 +86,27 @@ public class activity3_myitems extends AppCompatActivity {
                     Log.w(TAG, "Listen failed.", e);
                     return;
                 }
-                Log.d(TAG, "3");
                 //Add data from database into FoodList as custom object "FoodItem"
                 ArrayList<FoodItem> FoodList = new ArrayList<>();
                 for (QueryDocumentSnapshot doc : value) {
                     FoodItem newData = doc.toObject(FoodItem.class);
                     FoodList.add(newData);
                 }
-                Log.d(TAG, "4");
                 sort(FoodList);
-                Log.d(TAG, "5");
                 //Make new list of strings for displaying in app
-                displayList = new ArrayList<>();
+                itemList = new ArrayList<>();
                 for (FoodItem foodItem : FoodList){
-                    displayList.add(foodItem.getItem() + ": " + foodItem.getStringDate());
+                    itemList.add(foodItem.getItem());
+                    dateList.add(foodItem.getStringDate());
                 }
-                Log.d(TAG, Arrays.toString(displayList.toArray()));
+                Log.d(TAG, Arrays.toString(itemList.toArray()));
+                Log.d(TAG, Arrays.toString(dateList.toArray()));
 
-                ArrayAdapter arrayAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, displayList);
-                listView.setAdapter(arrayAdapter);
+                ArrayAdapter itemAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, itemList);
+                itemListView.setAdapter(itemAdapter);
+
+                ArrayAdapter dateAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, dateList);
+                dateListView.setAdapter(dateAdapter);
             }
         });
     }
